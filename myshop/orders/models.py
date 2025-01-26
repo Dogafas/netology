d33f0faf280026c282d4615b4d071fbfa821e62f
ByteBard <solomindavid10@gmail.com>
2025-01-26 17:59:26 +0700

    add order

---
+++ myshop/orders/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from phonenumbers import parse, is_valid_number, NumberParseException
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15, blank=True)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return f'Order {self.id}'
+    
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+    def clean(self):
+        super().clean()
+        # Проверка телефона
+        if self.phone:
+            try:
+                phone_obj = parse(self.phone, "RU")  # Проверяем в регионе Россия
+                if not is_valid_number(phone_obj):
+                    raise ValidationError({'phone': 'Введите корректный номер телефона.'})
+            except NumberParseException:
+                raise ValidationError({'phone': 'Введите корректный номер телефона.'})
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey('shop.Product', related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+    
+    def get_cost(self):
+        return self.price * self.quantity
