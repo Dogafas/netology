d784a811939a3b964fdef6c3191ceeb610080a94
ByteBard <solomindavid10@gmail.com>
2025-02-01 22:21:53 +0700

    last commit

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -5,6 +5,7 @@ from .models import Category, Product
 class CategoryAdmin(admin.ModelAdmin):
     list_display =['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
+    list_per_page = 8
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -12,4 +13,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
+    list_per_page = 8
 
