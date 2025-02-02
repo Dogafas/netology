d70f8bdaadc27674d66bf1a22a27235e25b57351
ByteBard <solomindavid10@gmail.com>
2025-02-02 22:55:26 +0700

    add sort

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,32 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from phones.models import Phone
 
 
 def index(request):
     return redirect('catalog')
 
-
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_by = request.GET.get('sort', 'name')
+
+    if sort_by == 'name':
+        phones = Phone.objects.all().order_by('name')
+    elif sort_by == 'price_asc':
+        phones = Phone.objects.all().order_by('price')
+    elif sort_by == 'price_desc':
+        phones = Phone.objects.all().order_by('-price')
+    elif sort_by == 'min_price': 
+          phones = Phone.objects.all().order_by('price') 
+    elif sort_by == 'max_price':
+        phones = Phone.objects.all().order_by('-price')
+    else:
+        phones = Phone.objects.all()
+    context = {'phones': phones, 'sort_by': sort_by}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = get_object_or_404(Phone, name=slug)
+    context = {'phone': phone}
     return render(request, template, context)
