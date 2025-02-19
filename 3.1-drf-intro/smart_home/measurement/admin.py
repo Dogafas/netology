bd4fb5f563a976f145a85edec7a18082fbf810e4
ByteBard <solomindavid10@gmail.com>
2025-02-20 01:37:08 +0700

    Добав. модели Sensor и Measurement с админской панелью и миграциями

--- 3.1-drf-intro/smart_home/measurement/admin.py
+++ 3.1-drf-intro/smart_home/measurement/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Sensor, Measurement
 
-# Register your models here.
+
+class MeasurementInline(admin.TabularInline):
+    model = Measurement
+    fields = []
+    extra = 3
+
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description")
+    list_per_page = 15
+    inlines = [MeasurementInline]
+    search_fields = ("name",)
+
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    def sensor_name(self, obj):
+        return obj.sensor.name
+
+    sensor_name.short_description = "Датчик"
+
+    list_display = ("sensor_name", "temperature", "updated_at")
+    list_filter = ("sensor", "updated_at")
+    list_per_page = 15
--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+# модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=50, verbose_name="название")
+    description = models.CharField(max_length=100, verbose_name="описание")
+
+    class Meta:
+        verbose_name = "Датчик"
+        verbose_name_plural = "Датчики"
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    id = models.AutoField(primary_key=True)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, verbose_name="датчик")
+    temperature = models.DecimalField(
+        max_digits=2, decimal_places=1, default=0.0, verbose_name="температура"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="время обновления")
+
+    class Meta:
+        verbose_name = "Измерение"
+        verbose_name_plural = "Измерения"
+
+    def __str__(self):
+        return f"{self.sensor.name} - {self.temperature}°C"
