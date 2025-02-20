941afab4153f0b3305e13a39465d889fb1fe92d9
ByteBard <solomindavid10@gmail.com>
2025-02-20 21:56:04 +0700

    Добав. документац. API с помощью drf_yasg

--- 3.1-drf-intro/smart_home/smart_home/settings.py
+++ 3.1-drf-intro/smart_home/smart_home/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your_secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "measurement",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
--- 3.1-drf-intro/smart_home/smart_home/urls.py
+++ 3.1-drf-intro/smart_home/smart_home/urls.py
@@ -18,11 +18,36 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Smart Home API",
+        default_version="v1",
+        description="API documentation for Smart Home",
+        terms_of_service="https://google.com/policies/terms",
+        contact=openapi.Contact(email="solomindavid10@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("measurement.urls")),
+    path(
+        "swagger<str:format>",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 
 if settings.DEBUG:
